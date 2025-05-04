85b46fb86ed5f822bb22e796bcc08f5c2217e762
Kris Nye <krisnye@gmail.com>
2025-05-04 15:23:42 -0700

    changed board link type

--- src/samples/twixt/services/state-service/create-state-service.ts
+++ src/samples/twixt/services/state-service/create-state-service.ts
@@ -3,13 +3,13 @@ import { addObservableResources } from "ecs/extensions/add-observable-resources"
 
 export type Player = "red" | "black";
 export type BoardPoint = Player | null;
-export type BoardLink = [number, number, number, number];
+export type BoardLink = [number, number]; // [fromIndex, toIndex]
 
 export function createStateService() {
     return addObservableResources(
         createDatabase(),
         {
-            board: new Array<BoardPoint>(12 ** 2).fill(null),
+            board: new Array<BoardPoint>(24 ** 2).fill(null),
             links: new Array<BoardLink>(0),
             hoverIndex: null as number | null,
         } as const
