d7df807aee051f84fc2e03cd038753348dfdd7dc
Kris Nye <krisnye@gmail.com>
2025-07-08 11:38:10 -0700

    updated to adobe/data 0.2.9

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,11 @@ export default defineConfig({
     },
     server: {
         port: 3000,
-        strictPort: true
+        strictPort: true,
+        headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedder-Policy': 'require-corp'
+        },
     },
     preview: {
         port: 3000,
